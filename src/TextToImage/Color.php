7384dc6714baf9a78e2ab31523b73bf2ae0c773c
Arkadiusz Kondas <arkadiusz.kondas@gmail.com>
2019-03-07 22:11:56 +0100

    Predict air pollution

---
+++ src/TextToImage/Color.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+
+namespace Akondas\TextToImage;
+
+/**
+ * Inspired by dracula theme
+ */
+final class Color
+{
+    public const BACKGROUND = [40, 42, 54];
+
+    public const FOREGROUND = [139, 233, 253];
+
+    public const SIGNATURE = [80, 250, 123];
+}
